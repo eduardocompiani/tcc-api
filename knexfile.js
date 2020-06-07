858481783030cca4d20c8c157a819e86482b5e2b
Eduardo Compiani <eduardovieiracompiani@gmail.com>
2020-06-06 23:48:27 -0300

    Permitir configurar porta

--- knexfile.js
+++ knexfile.js
@@ -31,6 +31,7 @@ module.exports = {
     client: 'postgresql',
     connection: {
       host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
       database: process.env.DB_DATABASE,
       user: process.env.DB_USER,
       password: process.env.DB_USE_PW
